0eec8f9fbfc7a810e30013a74527c72202191489
GYoung <ytok1108@aol.com>
2024-12-18 20:10:32 +0900

    C언어로 만든 간단한 guess game

---
+++ Rust_CS_Computer_System/001_By_the_C/a02_C_guess_game/README.md
@@ -0,0 +1,11 @@
+# Result
+
+```bash
+0 is not the right number
+Enter aother guess : 3
+3 is not the right number
+Enter aother guess : 2
+2 is not the right number
+Enter aother guess : 42
+Congratulations! Yu guessed the number : 42⏎ 
+```
---
+++ Rust_CS_Computer_System/001_By_the_C/a02_C_guess_game/src/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int num = 42;
+    int guess =0;
+
+    while (guess != num ) {
+        printf("%d is not the right number\n", guess);
+        printf("Enter aother guess : ");
+        scanf("%d", &guess);
+    }
+    printf("Congratulations! Yu guessed the number : %d", num);
+ 
+    return 0;
+}
