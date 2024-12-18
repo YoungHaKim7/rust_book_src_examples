8adf0e931faf83d0b36a436a9876cec58e650f1b
GYoung <ytok1108@aol.com>
2024-12-18 20:21:32 +0900

    function + void fn+ C언어

--- Rust_CS_Computer_System/001_By_the_C/a03_functions/src/main.c
+++ Rust_CS_Computer_System/001_By_the_C/a03_functions/src/main.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 int max(int x, int y) {
-    int bigger;
+  int bigger;
 
-    bigger = x;
-    if (y>x) {
-        bigger =y;
-    }
-    printf("  in max, before return x: %d y: %d\n", x, y);
-    return bigger;
+  bigger = x;
+  if (y > x) {
+    bigger = y;
+  }
+  printf("  in max, before return x: %d y: %d\n", x, y);
+  return bigger;
 }
 
 int main(void) {
 
-    int big_int = max(10, 3);
-    printf("max fn : %d", big_int);
- 
-    return 0;
+  int big_int = max(10, 3);
+  printf("max fn : %d", big_int);
+
+  return 0;
 }
